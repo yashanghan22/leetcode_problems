ba9efa1d464879257512481afbe9a7dbf0841f05
yashanghan22 <anghanyash222@gmail.com>
2024-07-30 21:04:13 +0530

    daily_july: 1653

---
+++ daily_problems/july/min_deletion_make_balanced_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        a_count_right=[0]*len(s)
+        for i in range(len(s)-2,-1,-1):
+            print(a_count_right[i+1],s[i+1])
+            a_count_right[i]=a_count_right[i+1]
+            a_count_right[i]+=1 if s[i+1]=='a' else 0
+        print(a_count_right)
+        b_count_left=0
+        res=len(s)
+        for i,c in enumerate(s):
+            res=min(res,b_count_left+a_count_right[i])
+            if c=='b':
+                b_count_left+=1
+        return res
+
+
+if __name__=='__main__':
+    s=Solution()
+    print(s.minimumDeletions('aababbab'))
