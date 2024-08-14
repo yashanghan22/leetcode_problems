7173c960e951ce3fb23af3e5def534a5a96407e8
yashanghan22 <anghanyash222@gmail.com>
2024-08-14 15:01:04 +0530

    daily_aug; 719

---
+++ daily_problems/august/find_kth_smallest_pair_dist.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        def helper(dist):
+            l=0
+            res=0
+            for r in range(len(nums)):
+                while nums[r]-nums[l]>dist:
+                    l+=1
+                res+=r-l
+            return res
+        l,r=0,max(nums)
+        while l<r:
+            m=l+((r-l)//2)
+            pairs=helper(m)
+            if pairs>=k:
+                r=m
+            else:
+                l=m+1
+        return r
+
+if __name__=="__main__":
+    s=Solution()
+    print(s.smallestDistancePair([1,3,1],1))
