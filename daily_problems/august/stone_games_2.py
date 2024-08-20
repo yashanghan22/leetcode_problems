0cd08edbf77e846516c953f99ae813f70858a8f5
yashanghan22 <anghanyash222@gmail.com>
2024-08-20 21:45:49 +0530

    daily_aug: 1140

---
+++ daily_problems/august/stone_games_2.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        dp={}
+
+        def dfs(alice,i,M):
+            if i==len(piles):
+                return 0
+            if (alice,i,M) in dp:
+                return dp[(alice,i,M)]
+            res=0 if alice else float('inf')
+            total=0
+            for X in range(1,2*M+1):
+                if i+X>len(piles):
+                    break
+                total+=piles[i+X-1]
+                if alice:
+                    res=max(res,total+dfs(not alice,i+X,max(M,X)))
+                else:
+                    res=min(res,dfs(not alice,i+X,max(M,X)))
+            dp[(alice,i,M)]=res
+            return res
+        return dfs(True,0,1)
+
+if __name__=='__main__':
+    s = Solution()
+    print(s.stoneGameII([2,7,9,4,4]))
