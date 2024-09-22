4e27997b8eda957d749f86c03b174c79a16be496
yashanghan22 <anghanyash222@gmail.com>
2024-09-22 18:51:18 +0530

    daily_sep: 386, 440

---
+++ daily_problems/september/lexicographical_number.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res=[]
+        cur=1
+        while len(res)<n:
+            res.append(cur)
+            if cur*10<=n:
+                cur*=10
+            else:
+                while cur==n or cur%10==9:
+                    cur=cur//10
+                cur+=1
+        return res
+
+if __name__=='__main__':
+    s=Solution()
+    print(s.lexicalOrder(10))
