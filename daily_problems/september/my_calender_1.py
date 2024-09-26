5102a4e723910c0fbc8d72125af13569d1b9971d
yashanghan22 <anghanyash222@gmail.com>
2024-09-26 09:55:18 +0530

    daily_sep: 729

---
+++ daily_problems/september/my_calender_1.py
@@ -0,0 +1,33 @@
+class Tree:
+    def __init__(self,start,end):
+        self.left=None
+        self.right=None
+        self.start=start
+        self.end=end
+    
+    def insert(self,start,end):
+        curr=self
+        while True:
+            if start>=curr.end:
+                if not curr.right:
+                    curr.right=Tree(start,end)
+                    return True
+                curr=curr.right
+            elif end<=curr.start:
+                if not curr.left:
+                    curr.left=Tree(start,end)
+                    return True
+                curr=curr.left
+            else:
+                return False
+
+class MyCalendar:
+
+    def __init__(self):
+        self.root=None
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.root:
+            self.root=Tree(start,end)
+            return True
+        return self.root.insert(start,end)
